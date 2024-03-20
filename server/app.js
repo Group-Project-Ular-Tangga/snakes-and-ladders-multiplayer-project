fcdb4e8434b7d7093313760dbc6f7dea96d852cb
Hackim18 <khakim668@gmail.com>
2024-03-20 21:23:20 +0700

    refactor: change port listener

--- server/app.js
+++ server/app.js
@@ -1,54 +1,56 @@
-const express = require('express');
-const { createServer } = require('http');
-const { Server } = require('socket.io');
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "*"
-    }
+  cors: {
+    origin: "*",
+  },
 });
 
 const finish = 100;
 
 let players = {};
 
-io.on('connection', (socket) => {
-    console.log(`Hello ${socket.id}`);
+io.on("connection", (socket) => {
+  console.log(`Hello ${socket.id}`);
 
-    socket.on('disconnect', () => {
-        console.log(`${socket.id} disconnected`);
-        if (socket.id in players) {
-            delete players[socket.id];
-            io.emit('updatePlayers', players);
-        }
-    });
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} disconnected`);
+    if (socket.id in players) {
+      delete players[socket.id];
+      io.emit("updatePlayers", players);
+    }
+  });
 
-    socket.on('joinGame', (playerName) => {
-        players[socket.id] = {
-            id: socket.id,
-            name: playerName,
-            position: 0
-        };
-        io.emit('updatePlayers', players);
-    });
+  socket.on("joinGame", (playerName) => {
+    players[socket.id] = {
+      id: socket.id,
+      name: playerName,
+      position: 0,
+    };
+    io.emit("updatePlayers", players);
+  });
 
-    socket.on('rollDice', () => {
-        const diceValue = Math.floor(Math.random() * 6) + 1;
-        const newPosition = players[socket.id].position + diceValue;
+  socket.on("rollDice", () => {
+    const diceValue = Math.floor(Math.random() * 6) + 1;
+    const newPosition = players[socket.id].position + diceValue;
 
-        if (newPosition >= finish) {
-            players[socket.id].position = finish;
-            io.emit('playerWins', players[socket.id]);
-        } else {
-            players[socket.id].position = newPosition;
-            io.emit('updatePlayers', players);
-        }
-    });
+    if (newPosition >= finish) {
+      players[socket.id].position = finish;
+      io.emit("playerWins", players[socket.id]);
+    } else {
+      players[socket.id].position = newPosition;
+      io.emit("updatePlayers", players);
+    }
+  });
 });
 
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;
